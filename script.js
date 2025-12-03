2029e791445219e8a606abd445740c534c1e01df
Aman Rajput <89723728+amanrajput12@users.noreply.github.com>
2025-12-03 21:25:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,46 +9,17 @@ function convertToRoman(num) {
       6:['I', 1]
     };
      let val = Number(num);
-	let rst="";
-  //your code here
-	while(val!=0){
-     if(val>=obj[0][1]){
-       rst+=obj[0][0];
-		val = val - obj[0][1]
+	  let val = Number(num);
+  let rst = "";
 
-	 }
+  for (let key in obj) {
+    while (val >= obj[key][1]) {
+      rst += obj[key][0];
+      val -= obj[key][1];
+    }
+  }
 
-	else	if(val>=obj[1][1]){
-       rst+=obj[1][0];
-		val = val - obj[1][1]
-	 }
-		else	if(val>=obj[2][1]){
-       rst+=obj[2][0];
-	val = val - obj[2][1]
-	 }
-
-		else	if(val>=obj[3][1]){
-       rst+=obj[3][0];
-		val = val - obj[3][1]
-	 }
-
-		else	if(val>=obj[4][1]){
-       rst+=obj[4][0];
-		val = val - obj[4][1]
-	 }
-
-		else	if(val>=obj[5][1]){
-       rst+=obj[5][0];
-		val = val - obj[5][1]
-	 }
-
-		else	if(val>=obj[6][1]){
-       rst+=obj[6][0];
-		val = val - obj[6][1]
-
-	 }
-	}
-	    return  rst
+  return rst;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
